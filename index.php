128324874f1509f9d3c8edd5c460a300e129fcea
FelipeMRamos <felipestudy@outlook.com>
2014-09-01 04:36:18 -0300

    add basic files for test
    an index with the main html
    an inc with the main php
    the zip with both

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<html>
+    <head>
+        <title>Tabela com Array</title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <table border='2'>
+            <tr>
+                <th>Turma</th>
+                <th>Disciplina</th>
+                <th>Semana</th>
+                <th>Professor</th>
+            </tr>
+            <?php
+                include "tabela.inc";
+            ?>
+        </table>
+    </body>
+</html>
